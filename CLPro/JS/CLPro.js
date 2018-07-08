a22bef69c9e18d318bed55a5b1eeb0759279b911
Dthall424 <hall.dylan46@yahoo.com>
2018-07-08 04:08:54 -0400

    trying to add a javascript function to complete the project, but I need help...

--- CLPro/JS/CLPro.js
+++ CLPro/JS/CLPro.js
@@ -1,2 +1,10 @@
-const $nameValue = $()
-const $submit =
+// const nameValue = document.getElementById("name")
+// const but = document.getElementById("submit")
+// $but.onclick = function() {
+//   console.log("Hello")
+//   alert("Hello" + nameValue.value + . + "Thanks for showing interest, I will get back to you ASAP!");
+// }
+
+$("#submit").click(function(){
+  console.log("Hello")
+});
